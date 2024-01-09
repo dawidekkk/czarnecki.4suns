d7539a15ac7201ff7166234718c0e4e4e9f2c333
Dawid Czarnecki <85833530+dawidekkk@users.noreply.github.com>
2024-01-09 19:34:45 +0100

    Update db_conn.php

--- config/db_conn.php
+++ config/db_conn.php
@@ -1,14 +1,12 @@
 <?php 
 
-$db['db_host'] = 'serwer1936460.home.pl';
-$db['db_user'] = '31300146_czarnecki';
-$db['db_pass'] = 'vKsO7Xd7';
-$db['db_name'] = '31300146_czarnecki';
+$db['db_host'] = '';
+$db['db_user'] = '';
+$db['db_pass'] = '';
+$db['db_name'] = '';
 
 foreach($db as $key=>$value) {
-  define(strtoupper($key), $value); // define function defines a CONSTANT (Constant value can't be changed after it is set.)
-  // also constant names don't need a DOLLAR SIGN($)! Or constants can be accessed regardless of scope. 
-  // echo 'key: '. $key . ' ' . 'value: ' . $value .'<br />';
+  define(strtoupper($key), $value); 
 }
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -16,11 +14,5 @@ $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if(!$connection) {
   echo "Connection error ";
   echo  mysqli_connect_error();
-  // echo '123';
 }
-
-// if($connection) {
-//   echo 'conffes';
-// }
-
 ?>
